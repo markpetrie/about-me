90fdce235516312f27ee574c3f03227a18f18c4a
Mark Petrie <petrie.mark@gmail.com>
2017-05-04 08:39:15 -0700

    Add favorite number guessing question

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 var correctAnswerCount = 0; // Used to keep track of the current correct answer count.
 
 // Capture user's name for personalization
-var userName = prompt('Welcome to my About Me page! Let\'s play a quick guessing game to help you get to know me a little better. I\'ll ask you 5 yes/no questions about myself and you try to guess the correct answers. First things first, please introduce yourself by entering your full name below.');
+var userName = prompt('Welcome to my About Me page! Let\'s play a quick game to help you get to know me a little better. I\'ll ask you 7 questions about myself and you try to guess the correct answers. First things first, please introduce yourself by entering your name below.');
 
-// Display personalized welcom alert
+// Display personalized welcome alert
 var personalWelcome = alert('Nice to meet you, ' + userName + '. Here comes your first question. Good luck!');
 
 //Begin question prompts and alerts
-var questionOne = prompt('Would I spend a work bonus on travel over a new laptop? [enter yes or no]');
+var questionOne = prompt('Would I spend my tax return on travel over a new laptop? [enter yes or no]');
 console.log('First question answer = ' + questionOne + '.');
 if(questionOne.toLowerCase() === 'y' || questionOne.toLowerCase() === 'yes') {
     correctAnswerCount = correctAnswerCount + 1; // increment correctAnswerCount by 1
@@ -19,18 +19,18 @@ else {
 }
 console.log('Current correct answer count = ' + correctAnswerCount);
 
-var questionTwo = prompt('Have I ever won the annual Seaside Beach volleyball tournament? [enter yes or no]');
+var questionTwo = prompt('Did I ever play competitive badminton? [enter yes or no]');
 console.log('Second question answer = ' + questionTwo + '.');
-if(questionTwo.toLowerCase() === 'y' || questionTwo.toLowerCase() === 'yes') {
+if(questionTwo.toLowerCase() === 'n' || questionTwo.toLowerCase() === 'no') {
     correctAnswerCount = correctAnswerCount +1; // increment correctAnswerCount by 1
-    alert('Correct! My doubles partner and I won the tournament in 1990. M.C. Hammer\'s \"U Can\'t Touch This\" was climbing the charts and I still had a full head of hair.');
+    alert('Correct! While I never played competitive badminton, I was a USAV ranked volleyball player winning my first major beach tournament in 1990.');
 }
 else {
-    alert('Wrong answer. My doubles partner and I won the tournament in 1990. M.C. Hammer\'s \"U Can\'t Touch This\" was climbing the charts and I still had a full head of hair.');
+    alert('No, it was not badminton. I was, however, a USAV ranked volleyball player winning my first major beach tournament in 1990.');
 }
 console.log('Current correct answer count = ' + correctAnswerCount); // increment correctAnswerCount by 1
 
-var questionThree = prompt('Do I speak any languages other than English?');
+var questionThree = prompt('Do I speak any languages other than English? [enter yes or no]');
 console.log('Third question answer = ' + questionThree + '.');
 if(questionThree.toLowerCase() === 'y' || questionThree.toLowerCase() === 'yes') {
     alert('Sorry. As much as I\'ve always wanted to learn another language, it\'s still just English for me right now.');
@@ -41,7 +41,7 @@ else {
 }
 console.log('Current correct answer count = ' + correctAnswerCount);
 
-var questionFour = prompt('Have I ever worked in a shipyard?');
+var questionFour = prompt('Have I ever worked in a shipyard? [enter yes or no]');
 console.log('Fourth question answer = ' + questionFour + '.');
 if(questionFour.toLowerCase() === 'y' || questionFour.toLowerCase() === 'yes') {
     correctAnswerCount = correctAnswerCount +1; // increment correctAnswerCount by 1
@@ -53,7 +53,7 @@ else {
 }
 console.log('Current correct answer count = ' + correctAnswerCount);
 
-var questionFive = prompt('Am I a web developer?');
+var questionFive = prompt('Am I a web developer? [enter yes or no]');
 console.log('Fifth question answer = ' + questionFive + '.');
 if(questionFive.toLowerCase() === 'y' || questionFive.toLowerCase() === 'yes') {
     correctAnswerCount = correctAnswerCount +1; // increment correctAnswerCount by 1
@@ -62,6 +62,45 @@ if(questionFive.toLowerCase() === 'y' || questionFive.toLowerCase() === 'yes') {
 else {
     alert('Actually, I am! Being a web developer is pretty awesome.');
 }
+console.log('Current correct answer count = ' + correctAnswerCount);
+
+// var questionSix = parseInt(prompt('What is my favorite number? [hint: it\'s between 0 and 10]'));
+// var favoriteNumber = 7;
+// console.log('Sixth question answer = ' + questionSix + '.');
+// if(questionSix === favoriteNumber) {
+//     correctAnswerCount = correctAnswerCount +1; // increment correctAnswerCount by 1
+//     alert('Yes, 7 is my favorite number. Good guess!');
+// }
+// else {
+//     alert('The number ' + questionSix + ' is fine and all, but 7 is my lucky number. Nice try!');
+// }
+// console.log('Current correct answer count = ' + correctAnswerCount);
+
+var maxGuesses = 4;
+var favoriteNumber = 7;
+
+for (var i = 0; i < maxGuesses; i++) {
+var questionSix = parseInt(prompt('What is my favorite number? [hint: it\'s between 0 and 10]'));
+if(questionSix === favoriteNumber) {
+    console.log('Sixth question answer = ' + questionSix + '.');
+    correctAnswerCount = correctAnswerCount +1; // increment correctAnswerCount by 1
+    alert('Yes, ' + favoriteNumber + ' is my favorite number! Nice job.');
+    break;    
+}
+else {
+    if(questionSix > favoriteNumber) {
+        console.log('Sixth question answer = ' + questionSix + '.');
+        alert('My favorite number is lower than ' + questionSix + '.');
+     }
+    
+     else {
+        console.log('Sixth question answer = ' + questionSix + '.');
+        alert('My favorite number is higher than ' + questionSix + '.');
+          }
+        }
+    }    
+
+    
 console.log('Current correct answer count = ' + correctAnswerCount);
 
 // display concluding alert message with the total number of correct answers 
